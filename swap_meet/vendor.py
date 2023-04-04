605406899c446de2ef3c8711f0f13e7a3962abe4
ryannff4 <ryannff4@gmail.com>
2023-04-03 17:02:31 -0700

    Wave1: first commit, implemented wave 1 code changes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    inventory = []
+
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        try:
+            self.inventory.remove(item)
+            return item
+        except ValueError:
+            return False
